72f60d7ff500eb67cd0d1380232b03107263a09e
rforcier2 <rforcier2@gmail.com>
2019-08-05 19:58:02 -0400

    Finishing up notely
    Finishing up notely. A Note maker app that simply allows users to create notes with titles and text. Date created is automatically current date

--- Notely/public/js/index.js
+++ Notely/public/js/index.js
@@ -1,6 +1,29 @@
 var app = new Vue({
     el: '#app',
         data: {
-            text: 'Hello all!'
+            title: 'Notely',
+            note: {
+                title: '',
+                text: ''
+            },
+
+            notes: [
+                {
+                    title: 'First Note',
+                    text: 'I loved my first note!',
+                    date: new Date(Date.now()).toLocaleString()
+                }
+            ]
+        },
+        
+        methods: {
+            addNote() {
+                let { text, title } = this.note;
+                this.notes.push({
+                    text, 
+                    title, 
+                    date: new Date(Date.now()).toLocaleString()
+                })
+            }
         }
     }); 
