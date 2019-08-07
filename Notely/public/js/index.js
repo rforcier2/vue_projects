60f1161fe2f48b2c7968607d759ea1b27009ccab
rforcier2 <rforcier2@gmail.com>
2019-08-07 08:31:42 -0400

    Updating CSS and functionalityq
    Updated close CSS, card, and card animations.
    Made a new option to add color to cards, and sort by color when you wish.

--- Notely/public/js/index.js
+++ Notely/public/js/index.js
@@ -2,28 +2,50 @@ var app = new Vue({
     el: '#app',
         data: {
             title: 'Notely',
+            currentColor: {
+                style: 'border',
+                text: "All"
+            },
+
             note: {
                 title: '',
-                text: ''
+                text: '',
+                style: '',
+                color: ''
             },
 
             notes: [
                 {
                     title: 'First Note',
                     text: 'I loved my first note!',
-                    date: new Date(Date.now()).toLocaleString()
+                    date: new Date(Date.now()).toLocaleString(),
+                    style: 'border-primary',
+                    color: 'Blue'
                 }
             ]
         },
         
         methods: {
             addNote() {
-                let { text, title } = this.note;
+                let { text, title, style, color } = this.note;
                 this.notes.push({
                     text, 
                     title, 
-                    date: new Date(Date.now()).toLocaleString()
+                    date: new Date(Date.now()).toLocaleString(),
+                    style,
+                    color
                 })
+            },
+
+            removeNote(index){
+                this.notes.splice(index, 1);
+            },
+
+            organizeNotesByColor(){
+                let select = document.getElementById("organizeSelect");
+                this.currentColor.style = select.value;
+                this.currentColor.text = select.options[select.selectedIndex].text;
             }
         }
+
     }); 
